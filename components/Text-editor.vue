303e600a2e7d6a7ee7ef849a9dc3bf85aa77628f
jonimes <jonimsfn@gmail.com>
2024-03-22 08:13:45 +0300

    init commit

---
+++ components/Text-editor.vue
@@ -0,0 +1,15 @@
+<script setup>
+import { ImageResize } from "quill-image-resize-module";
+
+const modules = {
+  name: "ImageResize",
+  module: ImageResize,
+};
+</script>
+<template>
+  <div>
+    <quill-editor theme="snow" toolbar="full" :modules="modules"></quill-editor>
+  </div>
+</template>
+
+<style scoped></style>
---
+++ pages/index.vue
@@ -0,0 +1,8 @@
+<script setup></script>
+<template>
+  <div>test
+    <TextEditor/>
+  </div>
+</template>
+
+<style scoped></style>
